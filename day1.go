03234acb1b07605599598d099743fa73bcbf023c
Matt Singletary <matt.singletary@gmail.com>
2020-12-01 14:10:29 -0500

    Solved first puzzle

---
+++ day1.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	err := run(os.Stdout, os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getTwo(nums []int) (int, int) {
+
+	// sort.Ints(nums)
+	// x+ y == 2020
+	fmt.Println("checking: ")
+	for i, x := range nums {
+		fmt.Printf("%d\n", x)
+		for j := i; j < len(nums); j++ {
+			y := nums[j]
+			fmt.Printf("\t%d\n", y)
+
+			if x+y == 2020 {
+				return x, y
+			}
+		}
+	}
+	return 0, 0
+}
+
+func run(w io.Writer, args []string) error {
+	// if len(args) != 0 {
+	// 	return errors.New("filename is hardcoded")
+	// }
+	file, err := os.Open("./day1/input.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	nums := []int{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			return err
+		}
+		nums = append(nums, num)
+	}
+
+	x, y := getTwo(nums)
+	fmt.Printf("x = %d\n", x)
+	fmt.Printf("y = %d\n", y)
+
+	result := x * y
+	fmt.Printf("x*y = %d\n", result)
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
+}
